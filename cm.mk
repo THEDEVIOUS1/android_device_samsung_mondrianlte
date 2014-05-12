c6be53bc0f1c79a090d32df9de081031822b4d3e
THEDEVIOUS1 <DEVIOUS7779311@YAHOO.COM>
2014-05-12 05:21:58 -0500

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/mondrianwifi/full_mondrianwifi.mk)
+$(call inherit-product, device/samsung/mondrianwifi/full_mondrianlte.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
@@ -8,10 +8,10 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=mondrianwifi \
-    TARGET_DEVICE=mondrianwifi \
-    BUILD_FINGERPRINT="samsung/mondrianwifixx/mondrianwifi:4.4.2/KOT49H/T320XXU1ANAI:user/release-keys" \
-    PRIVATE_BUILD_DESC="mondrianwifixx-user 4.4.2 KOT49H T320XXU1ANAI release-keys"
+    PRODUCT_NAME=mondrianlte \
+    TARGET_DEVICE=mondrianlte \
+    BUILD_FINGERPRINT="samsung/mondrianltexx/mondrianwifi:4.4.2/KOT49H/T325XXU1ANB7:user/release-keys" \
+    PRIVATE_BUILD_DESC="mondrianltexx-user 4.4.2 KOT49H T325XXU1ANB7 release-keys"
 
-PRODUCT_DEVICE := mondrianwifi
-PRODUCT_NAME := cm_mondrianwifi
+PRODUCT_DEVICE := mondrianlte
+PRODUCT_NAME := cm_mondrianlte
